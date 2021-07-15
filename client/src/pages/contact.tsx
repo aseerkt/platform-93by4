8dc6f6ce31bc56bc3d98c26874717f464f2eb43e
Nada Farook <nairah1619@gmail.com>
2021-07-15 16:49:41 +0530

    Contact page (design and copy changes)

--- client/src/pages/contact.tsx
+++ client/src/pages/contact.tsx
@@ -9,26 +9,30 @@ export default function Contact() {
     <Layout>
       <Flex mt={8} flexDir={['column-reverse', 'column-reverse', 'row']}>
         <Flex flexDir="column" justifyContent="space-between">
-          <Box p={[4, 5, 6]} pt={4}>
+          <Box p={[4, 5, 6]} pt={[8, 8, 32]} pb={8}>
             <Heading as="h1" size="xl" color={'white'} letterSpacing="tighter">
-              Have any queries / issues while subbmitting your portfolio ?
+              Have any issue with the platform?
             </Heading>
           </Box>
           <Flex flexDir="column" p={[4, 5, 6]}>
             <Text color={'gray.400'} fontSize="xl" fontWeight="medium">
-              Mail us at :
+              Contact us at :
             </Text>
             <Heading
               as="h1"
-              size="xl"
+              fontSize={['xl', '2xl', '4xl']}
+              py={[4, 2, 0]}
               color={'brand.500'}
-              fontWeight="extrabold"
               letterSpacing="tighter"
             >
-              <a href="mailto:neog@gmail.com">neog@gmail.com</a>
+              <a href="mailto:contact.neogcamp@gmail.com">
+                contact.neogcamp@gmail.com
+              </a>
             </Heading>
             <Text fontSize="lg" color={'gray.400'} mt={2}>
-              Queries will be resolved within 3 - 5 working days.
+              It’s the <strong>BEST WAY </strong>to reach us quickly. <br />
+              Queries will be resolved within{' '}
+              <strong>3 - 5 working days.</strong>
             </Text>
           </Flex>
           <Box p={[4, 5, 6]}>
